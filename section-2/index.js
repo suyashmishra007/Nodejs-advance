4622ffcda3a8386eed726be13e2e9e0b7682a8a9
Suyash <suyashmishra370@gmail.com>
2023-07-20 01:44:19 +0530

    before install pm2

--- section-2/index.js
+++ section-2/index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const app = express();
+process.env.UV_THREADPOOL_SIZE=1
 
-function doWork(duration){
-    const start = Date.now();
-    while(Date.now() - start < duration){}
+const cluster = require('cluster');
+const crypto = require('crypto');
+
+if (cluster.isPrimary) {
+    cluster.fork();
+    cluster.fork();
+} else {
+
+    const express = require('express');
+    const app = express();
+
+    // function doWork(duration) {
+    //     const start = Date.now();
+    //     while (Date.now() - start < duration) {}
+    // }
+
+    app.get('/', (req, res) => {
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Welcome Endpoint');
+        })
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('Fast API endpoint');
+    });
+
+    app.listen(8000, () => {
+        console.log('listening on http://localhost:8000');
+    })
 }
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Welcome')
-});
-
-app.listen(8000,()=>{
-    console.log('listening on http://localhost:8000');
-})
